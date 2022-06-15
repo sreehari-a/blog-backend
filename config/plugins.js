4a9583675355b18ec1dd81376452b695cd42da8a
Sreehari A <harisreehari1457@gmail.com>
2022-06-15 19:18:52 +0530

    issue fix

--- config/plugins.js
+++ config/plugins.js
@@ -1,17 +1,19 @@
-module.exports = ({ env }) => ({
+module.exports = ({ env }) => {
+  return {
     // ...
     upload: {
       config: {
-        provider: 'aws-s3',
+        provider: "aws-s3",
         providerOptions: {
-          accessKeyId: env('AWS_ACCESS_KEY_ID'),
-          secretAccessKey: env('AWS_ACCESS_SECRET'),
-          region: env('AWS_REGION'),
+          accessKeyId: env("AWS_ACCESS_KEY_ID"),
+          secretAccessKey: env("AWS_ACCESS_SECRET"),
+          region: env("AWS_REGION"),
           params: {
-            Bucket: env('AWS_BUCKET'),
+            Bucket: env("AWS_BUCKET_NAME"),
           },
         },
       },
     },
     // ...
-  });
+  };
+};
