198cbbdd66c03271573fac506003c272d6c30570
Sreehari A <harisreehari1457@gmail.com>
2022-06-15 17:19:48 +0530

    test s3

--- config/plugin.js
+++ config/plugin.js
@@ -1,4 +1,5 @@
 module.exports = ({ env }) => ({
+    // ...
     upload: {
       config: {
         provider: 'aws-s3',
@@ -12,4 +13,5 @@ module.exports = ({ env }) => ({
         },
       },
     },
+    // ...
   });
